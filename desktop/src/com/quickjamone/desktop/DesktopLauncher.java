753a133db7a36b148b51834f9fbfc3acaef198ca
Declan Whitford Jones <wjdeclan@gmail.com>
2016-11-19 15:01:07 -0500

    Default grade configurations and basic launch code

---
+++ desktop/src/com/quickjamone/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.quickjamone.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.quickjamone.QuickJam;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new QuickJam(), config);
+	}
+}
